39a23b9ed2aefde38d71b34289081117844d8a1a
abhishektiwari-knoldus <abhishek.tiwari1@knoldus.com>
2023-05-22 22:13:56 +0530

    commit by abhishek-tri

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Prabandhan.</title>
+        <title>Prabandhan</title>
         <?php require 'utils/styles.php';?>
       
         
